176331047592ee76b2647fd787fe6e6ade27a910
Mohammad amin Jafarian <amin.jafarian82@gmail.com>
2021-02-04 03:29:26 +0330

    fix missing locales

--- esx_upgraded_duty/locale/en.lua
+++ esx_upgraded_duty/locale/en.lua
@@ -1,6 +1,6 @@
 Locales['en'] = {
-	['busy'] = 'You went busy mod.',
-	['offbusy'] = 'You went on duty.',
+	['onduty'] = 'You went on duty.',
+	['offduty'] = 'You went off duty.',
   	['duty1'] = 'Press ~INPUT_CONTEXT~ to ~r~exit~s~ duty',
   	['duty2'] = 'Press ~INPUT_CONTEXT~ to ~g~enter~s~ duty',
 }
